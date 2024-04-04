003628a70a408cd86ddafe14ffd83bdb491ccb38
Miki Galdeano Rodriguez <mikigaldeanorodriguez@MacBook-Pro-de-Miki.local>
2024-04-04 17:48:24 +0200

    Update ronQI2Silvertest.java

--- src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
+++ src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
@@ -4,18 +4,31 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mps.ronqi2.RonQI2Silver;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mps.dispositivo.*;
 
-
+@ExtendWith(MockitoExtension.class)
 public class ronQI2SilverTest {
 
-  
+    @Mock(lenient = true)
+    public DispositivoSilver disp;
+    public RonQI2Silver ronq;
+
+    @BeforeEach
+    public void init(){
+        disp = Mockito.mock(DispositivoSilver.class);
+        ronq = new RonQI2Silver();
+    }
+
     /*
      * Analiza con los caminos base qué pruebas se han de realizar para comprobar que al inicializar funciona como debe ser. 
      * El funcionamiento correcto es que si es posible conectar ambos sensores y configurarlos, 
@@ -32,8 +45,7 @@ public class ronQI2SilverTest {
     @DisplayName("Al inicializar el sensor de presion retorna false")
     public void RonQI2Silver_inicializarSensorPresion_returnFalse() {
         // 1- Creamos el objeto Mock de la clase RonQI2Silver
-        DispositivoSilver disp = Mockito.mock(DispositivoSilver.class);
-        RonQI2Silver ronq = new RonQI2Silver();
+ 
         ronq.anyadirDispositivo(disp);
         // 2- Definimos el comportamiento
         when(ronq.disp.conectarSensorPresion()).thenReturn(false);
@@ -45,8 +57,7 @@ public class ronQI2SilverTest {
     @DisplayName("Al inicializar el sensor de presion retorna true y sensor de sonido retorna false")
     public void RonQI2Silver_inicializarSensorSonido_returnFalse() {
         // 1- Creamos el objeto Mock de la clase DispositivoSilver
-        DispositivoSilver disp = Mockito.mock(DispositivoSilver.class);
-        RonQI2Silver ronq = new RonQI2Silver();
+
         ronq.anyadirDispositivo(disp);
         // 2- Definimos el comportamiento
         when(ronq.disp.conectarSensorPresion()).thenReturn(true);
@@ -59,8 +70,7 @@ public class ronQI2SilverTest {
     @DisplayName("Al inicializar ambos sensores se conectan pero no se configura el de presion")
     public void RonQI2Silver_inicializarAmbosSensoresPeroNoConfigura_returnFalse() {
         // 1- Creamos el objeto Mock de la clase DispositivoSilver
-        DispositivoSilver disp = Mockito.mock(DispositivoSilver.class);
-        RonQI2Silver ronq = new RonQI2Silver();
+
         ronq.anyadirDispositivo(disp);
         // 2- Definimos el comportamiento
         when(ronq.disp.conectarSensorPresion()).thenReturn(true);
@@ -73,8 +83,7 @@ public class ronQI2SilverTest {
     @DisplayName("Al inicializar ambos sensores se conectan y si se configuran ambos")
     public void RonQI2Silver_inicializarAmbosSensoresYConfigura_returnTrue() {
         // 1- Creamos el objeto Mock de la clase DispositivoSilver
-        DispositivoSilver disp = Mockito.mock(DispositivoSilver.class);
-        RonQI2Silver ronq = new RonQI2Silver();
+
         ronq.anyadirDispositivo(disp);
         // 2- Definimos el comportamiento
         when(ronq.disp.conectarSensorPresion()).thenReturn(true);
@@ -85,21 +94,108 @@ public class ronQI2SilverTest {
         assertTrue(ronq.inicializar());
     }
 
+
+    @Test
+    @DisplayName("Al inicializar ambos sensores se conectan y si se configuran ambos")
+    public void RonQI2Silver_inicializarAmbosSensoresYConfiguraPresion_returnFalse() {
+        // 1- Creamos el objeto Mock de la clase DispositivoSilver
+
+        ronq.anyadirDispositivo(disp);
+        // 2- Definimos el comportamiento
+        when(ronq.disp.conectarSensorPresion()).thenReturn(true);
+        when(ronq.disp.conectarSensorSonido()).thenReturn(true);
+        when(ronq.disp.configurarSensorPresion()).thenReturn(true);
+        when(ronq.disp.configurarSensorSonido()).thenReturn(false);
+        // 3- Ejecutamos
+        assertFalse(ronq.inicializar());
+    }
+
+
+    @Test
+    @DisplayName("Al inicializar ambos sensores se conectan y si se configuran ambos")
+    public void RonQI2Silver_inicializarAmbosSensoresYConfiguraSonido_returnFalse() {
+        // 1- Creamos el objeto Mock de la clase DispositivoSilver
+
+        ronq.anyadirDispositivo(disp);
+        // 2- Definimos el comportamiento
+        when(ronq.disp.conectarSensorPresion()).thenReturn(true);
+        when(ronq.disp.conectarSensorSonido()).thenReturn(true);
+        when(ronq.disp.configurarSensorPresion()).thenReturn(false);
+        when(ronq.disp.configurarSensorSonido()).thenReturn(true);
+        // 3- Ejecutamos
+        assertFalse(ronq.inicializar());
+    }
+
     /*
      * Un reconectar, comprueba si el dispositivo desconectado, en ese caso, conecta ambos y devuelve true si ambos han sido conectados. 
      * Genera las pruebas que estimes oportunas para comprobar su correcto funcionamiento. 
      * Centrate en probar si todo va bien, o si no, y si se llama a los métodos que deben ser llamados.
      */
 
-     @Test
-     @DisplayName("Si se desconecta un sensor, se reconecta de manera exitosa")
-     public void RonQI2Silver_reconectar_returnTrue() {
+    @Test
+    @DisplayName("Si se desconecta un sensor, se reconecta de manera exitosa")
+    public void RonQI2Silver_noEstaDesconectado_returnFalse() {
         //1- Creamos el objeto Mok de la clase RonQI2Silver
-        RonQI2Silver ronq = Mockito.mock(RonQI2Silver.class);
-        DispositivoSilver disp = Mockito.mock(DispositivoSilver.class);
+
         ronq.anyadirDispositivo(disp);
         //2- Definimos el comportmaiento. Definimos un sensor no conectado.
+        when(ronq.disp.estaConectado()).thenReturn(true);
+        //4- Verificamos que se ha reconectado una vez
+       assertFalse(ronq.reconectar());
+    }
+
+     @Test
+     @DisplayName("Si se desconecta un sensor, se reconecta de manera exitosa")
+     public void RonQI2Silver_reconectarSoloPresion_returnFalse() {
+        //1- Creamos el objeto Mok de la clase RonQI2Silver
+        ronq.anyadirDispositivo(disp);
+        //2- Definimos el comportmaiento. Definimos un sensor no conectado.
+        when(ronq.disp.estaConectado()).thenReturn(false);
+        when(ronq.disp.conectarSensorPresion()).thenReturn(true);
+        when(ronq.disp.conectarSensorSonido()).thenReturn(false);
+        //4- Verificamos que se ha reconectado una vez
+       assertFalse(ronq.reconectar());
+     }
+
+
+     @Test
+     @DisplayName("Si se desconecta un sensor, se reconecta de manera exitosa")
+     public void RonQI2Silver_reconectarSoloSonido_returnFalse() {
+        //1- Creamos el objeto Mok de la clase RonQI2Silver
+
+        ronq.anyadirDispositivo(disp);
+        //2- Definimos el comportmaiento. Definimos un sensor no conectado.
+        when(ronq.disp.estaConectado()).thenReturn(false);
         when(ronq.disp.conectarSensorPresion()).thenReturn(false);
+        lenient().when(ronq.disp.conectarSensorSonido()).thenReturn(true);
+        //4- Verificamos que se ha reconectado una vez
+       assertFalse(ronq.reconectar());
+     }
+
+
+     @Test
+     @DisplayName("Si se desconecta un sensor, se reconecta de manera exitosa")
+     public void RonQI2Silver_reconectarAmbos_returnTrue() {
+        //1- Creamos el objeto Mok de la clase RonQI2Silver
+        ronq.anyadirDispositivo(disp);
+        //2- Definimos el comportmaiento. Definimos un sensor no conectado.
+        when(ronq.disp.estaConectado()).thenReturn(false);
+        when(ronq.disp.conectarSensorPresion()).thenReturn(true);
+        when(ronq.disp.conectarSensorSonido()).thenReturn(true);
+        //4- Verificamos que se ha reconectado una vez
+       assertTrue(ronq.reconectar());
+     }
+
+
+     @Test
+     @DisplayName("Si no se conecta ningún sensor después de desconectar el dispositivo, devuelve false.")
+     public void RonQI2Silver_reconectarNinguno_returnFalse() {
+
+        ronq.anyadirDispositivo(disp);
+        //2- Definimos el comportmaiento. Definimos un sensor no conectado.
+        when(ronq.estaConectado()).thenReturn(false);
+        when(ronq.disp.conectarSensorPresion()).thenReturn(false);
+        lenient().when(ronq.disp.conectarSensorSonido()).thenReturn(false);
         //4- Verificamos que se ha reconectado una vez
        assertFalse(ronq.reconectar());
      }
